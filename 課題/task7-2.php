46477f068d7f8b8db5bd68dad860f8d327d98b40
kiri0202 <kiri.ss0202@gmail.com>
2025-06-13 00:02:23 +0900

    6.13

--- 課題/task7-2.php
+++ 課題/task7-2.php
@@ -1,64 +1,65 @@
 <?php
 class Staff{
-  protected $name;
-  protected $age;
-  protected $sex;
-  private $id;
+  private $name;
+  private $age;
+  private $sex;
+  private static $id=0;
   
   public function __construct($name,$age,$sex){
     $this-> name = $name;
     $this-> age = $age;
     $this-> sex = $sex;
+    // $this->id=$id;
   }
+
   public function number(){
-    $a=0000;
-    while($a<10000){
-      $id="S.$a";
-      $a++;
-    }
+    self::$id++;
   }
-  public function show(){
-    printf("{$id}　{$this->name}　{$this->age}　{$this->sex} <br>");
+
+  public function show($prefix = 'S'){
+   
+      printf("(%s%04d) %s %d歳 %s",$prefix,self::$id,$this->name,$this->age,$this->sex);
+   
+    
+    // printf("{$id}　{$this->name}　{$this->age}　{$this->sex} <br>");
+    
   }
 }
-// 社員IDがわからない
-// 子クラス
 class PartStaff extends Staff{
-  private $part;
-  $jikyu=900;
-  public function __construct($name,$age,$sex,$part){
+  private $jikyu;
+  public function __construct($name,$age,$sex,$jikyu){
     parent::__construct($name,$age,$sex);
-    $this->part=$part;
+    $this->jikyu=$jikyu;
   }
- 
-  public function showPart(){
-    printf("{$id}　{$this->name}　{$this->age}　{$this->sex}　{$this->part} <br>");
+  public function show($prefix = 'S'){ 
+    // printf("{$id}　{$this->name}　{$this->age}　{$this->sex} <br>");
+    parent:: show('P');
+    printf(" 時給:%s",$this->jikyu);
+    
   }
-  public function show(){
-    printf("{$id}　{$this->name}　{$this->age}　{$this->sex}　{$this->part}$jikyu<br>");
-  }
-  
 }
 
-$staff=[];
-$staff[0]= new Staff("佐藤　一郎","31歳","男性");
-$staff[1]= new Staff("山田　花子","25歳","女性");
-$staff[2]= new Staff("鈴木　次郎","27歳","男性");
-$staff[3]= new PartStaff("田中　友子","24歳","女性","時給:");
-$staff[4]= new Staff("中村　三郎","27歳","男性");
+$staff=[]; 
+$staff[0]= new Staff("佐藤　一郎","31","男性");
+$staff[1]= new Staff("山田　花子","25","女性");
+$staff[2]= new Staff("鈴木　次郎","27","男性");
+$staff[3]= new PartStaff("田中　友子","24","女性",900);
+$staff[4]= new Staff("中村　三郎","27","男性");
 
-
-// function ALLStaff(staff $staff){
-//   $staff->show();
+foreach($staff as $staffs){
+ $staffs ->number();
+ $staffs ->show();
+ echo "<br>";
+}
+// foreach($partstaff as $partstaffs){
+//  $partstaffs ->number();
+//  $partstaffs ->show();
 // }
-// foreach($staffs as $staff){
-//   AllStaff($staff);
-// }
 
-$staff[0] ->show();
-$staff[1] ->show();
-$staff[2] ->show();
-$staff[3] ->showPart();
-$staff[4] ->show();
 
+
+// $staff[1] ->number();
+// $staff[1] ->show();
+// $staff[2] ->number();
+// $staff[2] ->show();
 ?>
